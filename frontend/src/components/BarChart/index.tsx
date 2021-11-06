649269645e15e682dc92791d1645d4a74330ddd6
Bruno Christopher <brunocmc2012@gmail.com>
2021-11-06 17:25:44 -0300

    Implementação de Gráficos Estáticos

---
+++ frontend/src/components/BarChart/index.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+const BarChart = () => {
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+    };
+    
+    const mockData = {
+        labels: {
+            categories: ['Vingadores', 'Liga da Justiça', 'Defensores', 'Guardiões', 'Agentes da Shield']
+        },
+        series: [
+            {
+                name: "% Sucesso",
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+            }
+        ]
+    };
+    return (
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={{...options, xaxis: mockData.labels}}
+              series={mockData.series}
+              type="bar"
+              width="500"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default BarChart;
---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+const DonutChart = () => {
+    const mockData = {
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Tony Stark', 'Steve Rogers', 'Clark Kent', 'Bruce Wayne', 'Nick Fury']
+    }
+    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+    return (
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={{...options, labels: mockData.labels}}
+              series={mockData.series}
+              type="donut"
+              width="500"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default DonutChart;
