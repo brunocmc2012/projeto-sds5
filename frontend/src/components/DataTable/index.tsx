45180bd6182e52c7ffe0f7ad7a879cdc8d0c1828
Bruno Christopher <brunocmc2012@gmail.com>
2021-11-07 13:40:42 -0300

    DataTable Integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,26 @@
-function DataTable(): any {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utilities/format";
+import { BASE_URL } from "utilities/requests";
+
+const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=10&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            })
+    }, [])
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,121 +34,15 @@ function DataTable(): any {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>12/06/2021</td>
-                        <td>Barry Allen</td>
-                        <td>12</td>
-                        <td>17</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>26/04/2021</td>
-                        <td>Clark Kent</td>
-                        <td>14</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>11/08/2021</td>
-                        <td>Bruce Wayne</td>
-                        <td>24</td>
-                        <td>15</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>16/03/2021</td>
-                        <td>Diana Prince</td>
-                        <td>01</td>
-                        <td>15</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>03/04/2021</td>
-                        <td>Tony Stark</td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>03/08/2021</td>
-                        <td>Thor Odinson</td>
-                        <td>04</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>22/06/2021</td>
-                        <td>Bruce Banner</td>
-                        <td>34</td>
-                        <td>05</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>12/07/2021</td>
-                        <td>Peter Parker</td>
-                        <td>12</td>
-                        <td>16</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>22/11/2021</td>
-                        <td>Clint Barton</td>
-                        <td>17</td>
-                        <td>15</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>16/05/2021</td>
-                        <td>Natasha Romanov</td>
-                        <td>14</td>
-                        <td>16</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>05/04/2021</td>
-                        <td>Steve Rogers</td>
-                        <td>52</td>
-                        <td>12</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>08/07/2021</td>
-                        <td>Nick Fury</td>
-                        <td>43</td>
-                        <td>32</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>22/06/2021</td>
-                        <td>Hank Pym</td>
-                        <td>26</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
